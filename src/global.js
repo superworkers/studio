88360d68638676b4c6f9942e5c5e92c31850d3f7
Matt Born <mattborn@users.noreply.github.com>
2026-02-19 11:54:18 -0700

    Let’s superwork!
    Refactored with nothing-cli
    
    2026-02-19 11:44:19 -0700 Sync w/ superwork.dev code
    2025-09-23 13:18:00 -0600 Let’s superwork!

---
+++ src/global.js
@@ -0,0 +1,35 @@
+const BRAND = 3
+const frames = document.querySelectorAll('#cycle img')
+let i = 0
+
+const swap = () => {
+  frames.forEach(s => s.classList.remove('active'))
+  frames[i % frames.length].classList.add('active')
+  i++
+}
+
+const cycle = () => {
+  if (i < 15) {
+    swap()
+    setTimeout(cycle, 200)
+  } else {
+    frames.forEach(s => s.classList.remove('active'))
+    frames[BRAND].classList.add('active')
+    setTimeout(() => {
+      i = 0
+      cycle()
+    }, 3000)
+  }
+}
+
+cycle()
+
+/* Tag */
+
+console.log(`███████ ██    ██ ██████  ███████ ██████  ██     ██  ██████  ██████  ██   ██
+██      ██    ██ ██   ██ ██      ██   ██ ██     ██ ██    ██ ██   ██ ██  ██
+███████ ██    ██ ██████  █████   ██████  ██  █  ██ ██    ██ ██████  █████
+     ██ ██    ██ ██      ██      ██   ██ ██ ███ ██ ██    ██ ██   ██ ██  ██
+███████  ██████  ██      ███████ ██   ██  ███ ███   ██████  ██   ██ ██   ██
+
+We want you! Join our Discord: https://discord.gg/ng8RNjm5Jz`)
